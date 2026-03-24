8ad2703b023a58fada4bbebd9de4ec5a7e45f0db
Sourav Das <souravdas007123@gmail.com>
2026-03-24 14:35:27 +0530

    update

--- authent/urls.py
+++ authent/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import login_view
+from .views import login_view,signup_view
 urlpatterns = [
     path('', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
 ]
--- authent/views.py
+++ authent/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_view(request):
     return render(request, 'authent/login.html')
+
+def signup_view(request):
+    return render(request, 'authent/signup.html')
